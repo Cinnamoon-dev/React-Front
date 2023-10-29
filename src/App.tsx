ed9808a38365eb8d75fbc0b3d899b33b513bce29
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-10-29 12:23:20 -0300

    chore: creating data

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { Table } from './components';
 import styles from './App.module.scss';
 
 function App() {
+  const data = [
+    {id: 1, name: "Pedro", email: "pedro@email.com"},
+    {id: 2, name: "Daniel", email: "daniel@email.com"},
+    {id: 3, name: "Guilherme", email: "guilherme@email.com"}
+  ]
   return (
     <div className={styles['App']}>
-      <Table/>
+      <Table tableData={data}/>
     </div>
   );
 }
