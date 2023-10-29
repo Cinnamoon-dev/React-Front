be0cf47ea68423aca78368b105068139dd1d276a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-10-29 14:12:50 -0300

    chore: importing Home page

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,8 +5,35 @@ type TableProps = {
     tableData: Object[]
 }
 
+type ButtonProps = {
+    type: "Edit" | "Delete"
+}
+
 const capitalize = (str: string) => {
     return str[0].toUpperCase() + str.slice(1)    
+}
+
+const Button = (props: ButtonProps) => {
+
+    const Style = {
+        "Edit": "#686e87",
+        "Delete": "#dc3545"
+    }
+
+    return(
+        <div className={styles['Button']}>
+            <button style={{backgroundColor: Style[props.type]}}>{props.type}</button>
+        </div>
+    )
+}
+
+const Actions = () => {
+    return(
+        <div style={{display: 'flex', gap: '7px'}}>
+            <Button type='Edit'/>
+            <Button type='Delete'/>
+        </div>
+    )
 }
 
 const Table = (props: TableProps) => {
@@ -25,7 +52,7 @@ const Table = (props: TableProps) => {
                 <tbody>
                     {props.tableData.map((e) => {
                         let values = Object.values(e).map((e) => <td>{e}</td>)
-                        values.push(<td><button>Edit</button><button>Delete</button></td>)
+                        values.push(<td><Actions/></td>)
                         return(<tr>{values}</tr>)
                     })}
                 </tbody>
