ec6c88cc333d351fe7369205cdbb38cdefcbcf3b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-10-29 12:22:56 -0300

    refactor: injecting dependency on data

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,23 +1,16 @@
 import styles from './Table.module.scss'
 
 
-type Data = {
-    id: number,
-    name: string,
-    email: string
+type TableProps = {
+    tableData: Object[]
 }
 
 const capitalize = (str: string) => {
     return str[0].toUpperCase() + str.slice(1)    
 }
 
-const Table = () => {
-    const tableData: Data[] = [
-        {id: 1, name: "Pedro", email: "pedro@email.com"},
-        {id: 2, name: "Daniel", email: "daniel@email.com"},
-        {id: 3, name: "Guilherme", email: "guilherme@email.com"}
-    ]
-    const titles = Object.keys(tableData[0]).map((e) => capitalize(e))
+const Table = (props: TableProps) => {
+    const titles = Object.keys(props.tableData[0]).map((e) => capitalize(e))
     
     return(
         <div className={styles['TableContainer']}>
@@ -25,12 +18,14 @@ const Table = () => {
                 <thead>
                     <tr>
                         {titles.map((e, i) => <th key={i}>{e}</th>)}
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 
                 <tbody>
-                    {tableData.map((e) => {
+                    {props.tableData.map((e) => {
                         let values = Object.values(e).map((e) => <td>{e}</td>)
+                        values.push(<td><button>Edit</button><button>Delete</button></td>)
                         return(<tr>{values}</tr>)
                     })}
                 </tbody>
