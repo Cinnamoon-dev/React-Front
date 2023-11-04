be08a9cb10da4c86953d42b742f870e8c85ad354
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-11-03 21:03:01 -0300

    feat: creating base component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import styles from './Input.module.scss'
+
+const Input = () => {
+    return(
+        <div className={styles['InputContainer']}>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Input
