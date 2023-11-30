22aef5245c4ec46583f632b386bddf0a69b01342
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-11-30 19:34:59 -0300

    feat: adding new props as they are needed

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,14 @@
 import styles from './Input.module.scss'
 
-const Input = () => {
+interface InputProps {
+    onChange?: React.Dispatch<React.SetStateAction<string | undefined>>
+    placeholder?: string
+}
+
+const Input = (props: InputProps) => {
     return(
         <div className={styles['InputContainer']}>
-            <input type="text" />
+            <input type="text" placeholder={props.placeholder} onChange={(e) => {props.onChange ? props.onChange(e.currentTarget.value) : console.log("")}}/>
         </div>
     )
 }
