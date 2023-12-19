51b1f09f312a7e8957b72aee0fa76557f837593a
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2023-12-19 16:41:09 -0300

    feat: adding onclick props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,15 @@
 import styles from './Input.module.scss'
 
 type InputProps = {
-    onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
+    onClick?: React.MouseEventHandler<HTMLInputElement>
     placeholder?: string
 }
 
 const Input = (props: InputProps) => {
     return(
         <div className={styles['InputContainer']}>
-            <input type="text" placeholder={props.placeholder} onChange={props.onChange}/>
+            <input type="text" placeholder={props.placeholder} onChange={props.onChange} onClick={props.onClick}/>
         </div>
     )
 }
